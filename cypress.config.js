bde951d088ba27a3c79b42b9d73de06808680ebc
kechadifarid <kechadifarid10@gmail.com>
2026-08-28 18:46:39 +0200

    fix

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,14 @@
+const allureWriter = require('@shelex/cypress-allure-plugin/writer');
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
 
   e2e: {
     setupNodeEvents(on, config) {
+            // plugin allure
+            allureWriter(on, config);
+      
+            return config 
         },
   },
 });
