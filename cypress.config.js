f82a16e608d3b3ce3aa3d9e720d69bb601cf7662
kechadifarid <kechadifarid10@gmail.com>
2026-08-28 18:43:53 +0200

    s

--- cypress.config.js
+++ cypress.config.js
@@ -1,18 +1,9 @@
-const allureWriter = require('@shelex/cypress-allure-plugin/writer');
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
-    env: {
-  allure: true,
-  allureResultsPath: 'allure-results',
-  allureReuseAfterSpec: true,
-  },
+
   e2e: {
     setupNodeEvents(on, config) {
-            // plugin allure
-            allureWriter(on, config);
-      
-            return config 
         },
   },
 });
